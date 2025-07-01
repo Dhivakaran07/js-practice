fbb6470ba2181202be6bfa1d5a6be610aa83959b
dhivakaran07 <dhivamoorthy735@gmail.com>
2025-07-01 15:20:41 +0530

    Add script.js for array methods

---
+++ script.js
@@ -0,0 +1,13 @@
+//array methods
+let students=['dhiva','nithish','kathir','sanjay'];
+//1.push
+// console.log(students.push('jai'));
+// console.log(students.pop());
+// console.log(students.unshift('jai'))
+// console.log(students.shift());
+// console.log(students.indexOf('nithish'));
+// console.log(students.includes('jai'));
+// // console.log(students);
+// console.log(students.slice(2,5));
+console.log(students.splice(1,2,'wakali'))
+console.log(students)
