244fe83c7783ef45ca0c2077e20f812a0b8a0144
Dhivakaran07 <dhivamoorthy735@gmail.com>
2025-08-04 12:44:21 +0530

    Geo location

--- script.js
+++ script.js
@@ -1,43 +1,12 @@
 /*
-Sets is a collection of unique values it cannot have dupilicates
-the method of 
-1.add()
-2.delete()
-3.has()
-4.size()
-
+This code uses the Geolocation API to get the current location (latitude and longitude) of the user. It asks for permission in the browser and returns the coordinates if the user allows.
 
 */ 
 
-/* let fruits=new Set();
-fruits.add("apple");
-fruits.add("banana")
-console.log(fruits)
-console.log(fruits.size)
-fruits.delete("apple")
-console.log(fruits)
-console.log(fruits.has("banana"))
-let numbers=new Set([1,2,3,4,5,5])
-for(let fruit of numbers){
-    console.log(fruit)
+navigator.geolocation.getCurrentPosition((position)=>{
+    console.log(position)
+},
+(err)=>{
+    console.log(err)
 }
-
-*/
-
-// why we go with maps means it is faster and then it gurantee in order in object no  any type of keys are allowed in maps
-
-
-let fruits=new Map();
-fruits.set('apple',"$100")
-fruits.set('banana',"$100")
-fruits.set('orange',"$100")
-console.log(fruits);
-console.log(fruits.get("apple"));
-console.log(fruits.has("banana"));
-console.log(fruits.clear())
-console.log(fruits)
-
-
-
-
-
+);
