05d711ca6e1b6a7df92df3137d83691f7cf41bed
Dhivakaran07 <dhivamoorthy735@gmail.com>
2025-08-04 23:56:05 +0530

    async and await

--- script.js
+++ script.js
@@ -1,43 +1,40 @@
 /*
-Sets is a collection of unique values it cannot have dupilicates
-the method of 
-1.add()
-2.delete()
-3.has()
-4.size()
+
+
+async is use to returns the promises 
+awit is use to it waits when promises print then it is print
 
 
 */ 
-
-/* let fruits=new Set();
-fruits.add("apple");
-fruits.add("banana")
-console.log(fruits)
-console.log(fruits.size)
-fruits.delete("apple")
-console.log(fruits)
-console.log(fruits.has("banana"))
-let numbers=new Set([1,2,3,4,5,5])
-for(let fruit of numbers){
-    console.log(fruit)
+ 
+// it is nested function
+function later(){
+  return new Promise((resolve)=>{
+  setTimeout(()=>{
+  let discussion=true;
+  if(discussion){
+  resolve("it is successfull")
+  }
+  },3000)
+})};
+ async function after(){
+  console.log("what is the discusion")
+  let result= await later()
+  console.log(result)
+  console.log("the discussion is finished")
 }
+after()
 
-*/
+// without nested we have to nested we use
 
-// why we go with maps means it is faster and then it gurantee in order in object no  any type of keys are allowed in maps
-
-
-let fruits=new Map();
-fruits.set('apple',"$100")
-fruits.set('banana',"$100")
-fruits.set('orange',"$100")
-console.log(fruits);
-console.log(fruits.get("apple"));
-console.log(fruits.has("banana"));
-console.log(fruits.clear())
-console.log(fruits)
-
-
-
-
-
+/* async function nonest(){
+ let result=  new   Promise((resolve=>{
+    discussion=true;
+    if(discussion){
+   resolve("it is succes")  //in this we does not use the await but it is fullfilled because we not use the timers so it prints . we use the timers means is pending
+    }
+  }))
+    console.log(result)
+}
+  nonest()
+  */
