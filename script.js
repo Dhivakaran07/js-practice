ab47391c8ff238504ab8448e6578198e949c88e1
Dhivakaran07 <dhivamoorthy735@gmail.com>
2025-08-04 23:55:21 +0530

    sets and maps

--- script.js
+++ script.js
@@ -1,51 +1,43 @@
-/*Constructor used to create a object in this where we can we You create as many objects as you want 🔁
-You can pass different values for each object
-Easy to manage and clean code
-The constructor Person is a blueprint, and p1 is the real object (built using the blueprint)
-
-Class:A class is a blueprint for creating objects.
-It defines properties and methods that the objects will have.
-Methods:A method is a function inside an object or class.
-It performs actions using that object's data.
-*/ 
-
 /*
-function Information(name, age) {
-    this.EmpName = name;
-    this.EmpAge = age;
-}
+Sets is a collection of unique values it cannot have dupilicates
+the method of 
+1.add()
+2.delete()
+3.has()
+4.size()
 
-// ✅ Add method using prototype (shared by all instances)
-Information.prototype.details = function () {
-    return `The employee name is ${this.EmpName}, and the age is ${this.EmpAge}`;
-};
-
-Information.prototype.info = function () {
-    console.log(`The employee name is ${this.EmpName}, and the age is ${this.EmpAge}`);
-};
-
-let p1 = new Information('Dhivakaran', 19);
-let p2 = new Information('hello', 19);
-
-// Check if the function is shared
-console.log(p1.details === p2.details); // true ✅
-
-console.log(p1.details()); // "The employee name is Dhivakaran, and the age is 19"
-console.log(p2.details()); // "The employee name is hello, and the age is 19"
-p1.info(); // Logs: The employee name is Dhivakaran, and the age is 19
 
 */ 
 
-let details={
-    
- 
-    info:function(){
-        console.log(`hello ${this.name},${this.age}`)
-    }
+/* let fruits=new Set();
+fruits.add("apple");
+fruits.add("banana")
+console.log(fruits)
+console.log(fruits.size)
+fruits.delete("apple")
+console.log(fruits)
+console.log(fruits.has("banana"))
+let numbers=new Set([1,2,3,4,5,5])
+for(let fruit of numbers){
+    console.log(fruit)
 }
 
-let res=Object.create(details)
-console.log(res)
-res.name="dhiva"
-console.log(res.name)
+*/
+
+// why we go with maps means it is faster and then it gurantee in order in object no  any type of keys are allowed in maps
+
+
+let fruits=new Map();
+fruits.set('apple',"$100")
+fruits.set('banana',"$100")
+fruits.set('orange',"$100")
+console.log(fruits);
+console.log(fruits.get("apple"));
+console.log(fruits.has("banana"));
+console.log(fruits.clear())
+console.log(fruits)
+
+
+
+
 
